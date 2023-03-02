55f98b8175b2cb89c69d3af32df95cc350805526
Melissa Morris <mel.morris2021@gmail.com>
2023-03-02 10:30:04 -0500

    Adding images

---
+++ AngularDemo/src/app/mock-pokemon.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from './pokemon';
+
+export const POKEMON: Pokemon[] = [
+  { id: 1, name: 'Bulbasaur', picture: '../assets/bulbasaur.png'},
+  { id: 2, name: 'Ivysaur', picture: '../assets/ivysaur.png'},
+  { id: 3, name: 'Venusaur', picture: '/assets/venusaur.jpg'},
+];
--- AngularDemo/src/app/pokemon/pokemon.component.ts
+++ AngularDemo/src/app/pokemon/pokemon.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {POKEMON } from '../mock-pokemon';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.scss']
+  styleUrls: ['./pokemon.component.scss'],
 })
 
 export class PokemonComponent {
-  pokemon = 'Bulbasaur';
+  pokemon = POKEMON;
 }
 
