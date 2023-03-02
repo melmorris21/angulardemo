b518a51e9f64f39dc66165149fe489458ff20ad6
Melissa Morris <mel.morris2021@gmail.com>
2023-03-02 09:24:19 -0500

    Initial commit with basic functionality

--- AngularDemo/src/app/app.component.ts
+++ AngularDemo/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularDemo';
+  title = 'Pokemon Battle Station';
+  footer ='2023 | Melissa Morris';
 }
